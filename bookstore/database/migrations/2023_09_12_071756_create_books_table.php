3368bd0ade0a59e282e18e66b7a83be44168bc63
Christuss <szendefi.krisztofer@gmail.com>
2023-09-12 10:26:19 +0200

    insert records

--- bookstore/database/migrations/2023_09_12_071756_create_books_table.php
+++ bookstore/database/migrations/2023_09_12_071756_create_books_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Book;
 
 return new class extends Migration
 {
@@ -18,6 +19,22 @@ return new class extends Migration
             $table->integer('pieces')->default(50);
             $table->timestamps();
         });
+
+        Book::create([
+            'author' => "Béla",
+            'title' => 'Béla kaladjai',
+            'pieces' => 4
+        ]);
+        Book::create([
+            'author' => "Jani",
+            'title' => 'Jani kaladjai',
+            'pieces' => 6
+        ]);
+        Book::create([
+            'author' => "Józsi",
+            'title' => 'Józsi kaladjai',
+            'pieces' => 102
+        ]);
     }
 
     /**
